fd0678c9b5de489516a73a8d1d6f970a4f941661
fspjonny <fabiosilvapedro@yahoo.com.br>
2023-05-15 15:57:42 -0300

    first files

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<template>
+  <Home/>
+</template>
+
+<script setup>
+import Home from './pages/Home.vue'
+</script>
+
+<style scoped>
+</style>
---
+++ src/pages/Home.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <h2>Página inicial</h2>
+        <hr>
+        Contagem atual: {{ counter.count }}
+        <button @click="counter.increment">+</button>
+        <button @click="counter.decrement">-</button>
+    </div>
+</template>
+
+<script setup>
+import { useCounterStore } from '../stores/CounterStore'
+
+const counter = useCounterStore()
+
+counter.count++
+counter.$patch({count: counter.count + 1})
+counter.increment()
+
+</script>
+
+<style scoped>
+
+</style>
